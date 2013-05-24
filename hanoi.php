392100cdaaf0a39b339b61a7b1d87096bb0fffb4
febr <febr@stu.edu.tw>
2013-05-24 21:33:23 +0800

    Modify if condition

--- hanoi.php
+++ hanoi.php
@@ -3,19 +3,18 @@
  * Towers Of Hanoi
  */
 function prtAction($n, $from, $to) {
-    //print $n . 'move from:' . $from . 'to' . $to . '\n';
-    echo $n . 'move from:' . $from . 'to' . $to . "\n";
+    echo $n . " : " . $from . " to " . $to . "\n";
 }
 
 function hanoi($n, $start, $temp, $end){
-    if( $n > 1 ) {
+    if( $n > 0 ) {
         hanoi($n-1, $start, $end, $temp);
         prtAction($n, $start, $end);
         hanoi($n-1, $temp, $start, $end);
     }
 }
 
-$n = 4;
+$n = 3;
 $start = 'A';
 $temp = 'B';
 $end = 'C';
