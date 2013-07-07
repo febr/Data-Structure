592909435f5483d4658f83d431dbad3175bde920
febr <febr@stu.edu.tw>
2013-07-07 16:53:56 +0800

    Add BFS

---
+++ BFS.php
@@ -0,0 +1,47 @@
+<?php
+
+//Map path data
+$map = array(
+        array(0,1,1,0,0,0,0,0)
+      , array(0,0,0,1,1,0,0,0)
+      , array(0,0,0,0,0,1,1,0)
+      , array(0,0,0,0,0,0,0,0)
+      , array(0,0,0,0,0,0,0,0)
+      , array(0,0,0,0,0,0,0,0)
+      , array(0,0,0,0,0,0,0,1)
+      , array(0,0,0,0,0,0,0,0)
+  );
+
+//Parent point
+$parentArr = array();
+array_push($parentArr, $map[0]);
+
+//queue
+$temp = array(0);
+
+while(!empty($temp)) {
+    //current parent point
+    $parentArr = $temp;
+
+    //clear queue
+    $temp = array();
+
+    //searching
+    foreach($parentArr as $value){
+        foreach($map[$value] as $keyV =>$valueV){
+            if($valueV == 1) {
+                array_push($temp, $keyV);
+            }
+        }
+    }
+
+    //print
+    foreach($parentArr as $value){
+        echo $value."->";
+    }
+
+    //layer
+    echo "\n";
+}
+
+?>
